dd774a632176fec450b424cf87b7bd0ebefa852a
Anand <anandangalig@gmail.com>
2016-10-05 17:02:17 -0400

    create frontend for Community

--- app/app.php
+++ app/app.php
@@ -18,23 +18,39 @@
     Request::enableHttpMethodParameterOverride();
 
     $app->get("/", function() use ($app) {
-        return $app["twig"]->render("index.html.twig");
+        return $app["twig"]->render("index.html.twig", array("current_members" => User::getAll()));
     });
 
-    $app->get("/start_sharing", function() use ($app) {
+//=================Members:====================================
+    $app->get("/members", function() use ($app) {
         return $app["twig"]->render("profiles.html.twig");
     });
+
 
     $app->post("/profile_create", function() use ($app) {
         $name = $_POST['new_member_name'];
         $email = $_POST['new_member_email'];
         $new_member = new User($name, $email);
         $new_member->save();
-        return $app["twig"]->render("profiles.html.twig", array(User::getAll()));
+        return $app["twig"]->render("profiles.html.twig", array("current_members" => User::getAll()));
     });
 
+    $app->get("/delete_all_members", function() use ($app) {
+        User::deleteAll();
+        return $app["twig"]->render("profiles.html.twig", array("current_members" => User::getAll()));
+    });
 
+//==================Communities:================================
 
+    $app->get("/communities", function() use ($app) {
+        return $app["twig"]->render("communities.html.twig", array("current_communities" => Community::getAll()));
+    });
+    $app->post("/community_create", function() use ($app) {
+        $name = $_POST['new_community_name'];
+        $new_community = new Community($name);
+        $new_community->save();
+        return $app["twig"]->render("communities.html.twig", array("current_communities" => Community::getAll()));
+    });
 
     return $app;
  ?>
